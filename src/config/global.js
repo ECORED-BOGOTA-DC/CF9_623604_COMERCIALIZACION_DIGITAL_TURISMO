b1ade8b8c70c9bb95580179d8179ba9be5896c0f
Luis Perez <ljpm9401@gmail.com>
2023-11-29 19:55:16 -0500

    Ajustes

--- src/config/global.js
+++ src/config/global.js
@@ -309,7 +309,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/623604_CF09_DU.pdf',
       },
       {
         icono: 'fas fa-download',
